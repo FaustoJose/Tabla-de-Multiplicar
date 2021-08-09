b9c8c7a714933d1eacf0fac158d078d0660b088f
Fausto Jose Tineo Mejia <2016-2155@est.itsc.edu.do>
2021-08-08 21:00:03 -0400

    Agregar archivos de proyecto.

---
+++ Tabla de Multiplicar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tabla_de_Multiplicar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tablaMultiplicar(5);
+        }
+        public static void tablaMultiplicar(int num)
+        {
+            int rus;
+            for (int i=0;i<=10;i++) 
+            {
+                rus = num * i;
+                Console.WriteLine(num+" x "+i+" = "+rus);
+            }
+        }
+    }
+}
